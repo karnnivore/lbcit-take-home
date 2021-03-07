0c18686dfd310d24a65fe8b0318aebbf045118c0
Nick Chinsen <nicholaschinsen@gmail.com>
2021-03-07 18:46:08 -0500

    changed color

--- src/components/three/CustomBox.tsx
+++ src/components/three/CustomBox.tsx
@@ -48,8 +48,8 @@ export default function CustomBox(props: any) {
         <CustomText position={[-20, 2, 0]} text='New York' color='red' setTableBold={props.setNewYork} link="https://weather.com/weather/tenday/l/f892433d7660da170347398eb8e3d722d8d362fe7dd15af16ce88324e1b96e70"/>
         <CustomText position={[5, 5, 0]} text='London' color='blue' setTableBold={props.setLondon} link="https://weather.com/weather/tenday/l/7517a52d4d1815e639ae1001edb8c5fda2264ea579095b0f28f55c059599e074"/>
         <CustomText position={[5, 1, 4]} text='Berlin' color='green' setTableBold={props.setBerlin} link="https://weather.com/weather/tenday/l/Berlin+Germany?canonicalCityId=49875d033efa3f8a515cc07b7ab1897df30ce65616175ffb4591cb95c5f646ff"/>
-        <CustomText position={[-15, -2, 0]} text="Nick" color='pink' setTableBold={props.setTokyo} link="https://nickchinsen.com"/>
-        <CustomText position={[6, -2, 0]} text="Chinsen" color='pink' setTableBold={props.setTokyo} link="https://nickchinsen.com"/>
+        <CustomText position={[-15, -2, 0]} text="Nick" color='black' setTableBold={props.setTokyo} link="https://nickchinsen.com"/>
+        <CustomText position={[6, -2, 0]} text="Chinsen" color='black' setTableBold={props.setTokyo} link="https://nickchinsen.com"/>
         {/* Display custom spinning box */}
         <SpinningBox position={[-1.2, 0, 0]} color='purple' setTableBold={props.setTokyo} link="https://weather.com/weather/tenday/l/4ba28384e2da53b2861f5b5c70b7332e4ba1dc83e75b948e6fbd2aaceeeceae3"/>
         <SpinningBox position={[2.1, 0, 0]} color='red' setTableBold={props.setNewYork} link="https://weather.com/weather/tenday/l/f892433d7660da170347398eb8e3d722d8d362fe7dd15af16ce88324e1b96e70"/>
