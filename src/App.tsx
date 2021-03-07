e24b37f3fe334b22baece5ef53607952370bd3b7
Nick Chinsen <nicholaschinsen@gmail.com>
2021-03-07 17:35:31 -0500

    Added loading & fetch data from api

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,35 @@
-import React, { useRef, useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.scss';
-import CustomBox from './components/CustomBox';
-import Table from './components/Table';
+import CustomBox from './components/three/CustomBox';
+import MyTable from './components/table/MyTable';
 function App() {
+  const [loading, isLoading] = useState(true);
+  const [tableData, setTableData] = useState([]);
+
+  //load data from api
+  useEffect(() => {
+    const dataFetch = async () => {
+      const url = 'https://www.lbcit.ca/demo/api/?key=d9658b9d-4f86-491f-bd67-86af0c547a5c';
+      const response = await fetch(url);
+      const data = await response.json();
+      setTableData(data)
+      isLoading(false)
+    }
+
+    dataFetch();
+  }, [])
+
   return (
-    //React fragment containing canvas which contains three js elements
     <>
-      <Table/>
-      <CustomBox/>
+      {//if loading or no table data display loading 
+       loading || !tableData ? 
+        <div>Loading</div>
+        :
+        <>
+          <MyTable tableData={tableData}/>
+          <CustomBox/>
+        </>
+      }
     </>
   );
 }
